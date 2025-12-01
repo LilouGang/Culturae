73f1284842f11c98c86f2278b6999f68d7a12cdb
Killian Lacaque <killian@MacBook-Air-de-Killian.local>
2025-12-02 00:36:55 +0100

    Build: Integrate build command into Vercel script

--- vercel-install.sh
+++ vercel-install.sh
@@ -6,7 +6,12 @@ FLUTTER_VERSION="3.35.2"
 # On clone la branche de cette version spécifique
 git clone https://github.com/flutter/flutter.git --branch $FLUTTER_VERSION --depth 1
 
-# Le reste du script est identique
+# On ajoute Flutter au PATH
 export PATH="$PATH:`pwd`/flutter/bin"
+
+# On installe Flutter et les dépendances du projet
 flutter precache
 flutter pub get
+
+# --- ON AJOUTE LA COMMANDE DE BUILD ICI ---
+flutter build web --release
